bbf500afc98c68ab87fba516ffa65a9f19aac27e
Dan Kochetov <danil.kochetov@gmail.com>
2026-06-10 15:31:09 +0300

    Remove 'on_hold' status

--- schema.ts
+++ schema.ts
@@ -1,6 +1,6 @@
 import { pgEnum, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
-export const userStatus = pgEnum('user_status', ['active', 'archived', 'suspended', 'deleted', 'on_hold', 'trialing']);
+export const userStatus = pgEnum('user_status', ['active', 'archived', 'suspended', 'deleted', 'trialing']);
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
